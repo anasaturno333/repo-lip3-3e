87f480c3d79331e160abb2ec6abf58426fc64924
Ana Carolina Saturno Tabosa <rm78956@estudante.fieb.edu.br>
2022-03-18 10:54:38 -0300

    Primeiro Commit

---
+++ src/ResultadoAluno.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class ResultadoAluno {
+
+	public static void main(String[] args) {
+
+		double media = 6.0;
+
+		if (media >= 6.0) {
+			JOptionPane.showMessageDialog(null, "aprovado!");
+		} else
+			JOptionPane.showMessageDialog(null, "Reprovado!");
+
+	}
+}
